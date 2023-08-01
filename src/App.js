6a2f4016f62cc75af91e6798fa3d1e65a7c7b0aa
Daniel Burgess <dan-burgess@hotmail.com>
2023-08-01 22:09:23 +0930

    Create Heeader component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import logo from './assets/investment-calculator-logo.png';
 
+
+const Header = (props) => {
+  return (
+    <header className="header">
+      <img src={props.logo} alt="logo" />
+      <h1>Investment Calculator</h1>
+    </header>
+  )
+}
+
 function App() {
   const calculateHandler = (userInput) => {
     // Should be triggered when form is submitted
@@ -30,10 +40,7 @@ function App() {
 
   return (
     <div>
-      <header className="header">
-        <img src={logo} alt="logo" />
-        <h1>Investment Calculator</h1>
-      </header>
+      <Header logo={logo} />
 
       <form className="form">
         <div className="input-group">

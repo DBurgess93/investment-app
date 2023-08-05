50e6f4174524b9c55916d99c3f6d7e6639d709ec
Daniel Burgess <dan-burgess@hotmail.com>
2023-08-06 09:04:59 +1000

    input change handlers

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const Form = () => {
     console.log('RESET')
   }
   const changeHandler = (input, value) => {
-    console.log('yo')
+    console.log(input, value)
   }
   return (
     <form className="form" onSubmit={submitHandler}>
@@ -34,7 +34,10 @@ const Form = () => {
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input onChange={changeHandler} type="number" id="yearly-contribution" />
+          <input onChange={(event) =>
+            changeHandler('yearly-contribution', event.target.value)}
+            type="number"
+            id="yearly-contribution" />
         </p>
       </div>
       <div className="input-group">
@@ -42,11 +45,17 @@ const Form = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input onChange={changeHandler} type="number" id="expected-return" />
+          <input onChange={(event) =>
+            changeHandler('expected-return', event.target.value)}
+            type="number"
+            id="expected-return" />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input onChange={changeHandler} type="number" id="duration" />
+          <input onChange={(event) =>
+            changeHandler('duration', event.target.value)}
+            type="number"
+            id="duration" />
         </p>
       </div>
       <p className="actions">

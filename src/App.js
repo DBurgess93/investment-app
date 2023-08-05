0e66e88666d3c3ee164119390bc97488ff37d028
Daniel Burgess <dan-burgess@hotmail.com>
2023-08-06 09:01:13 +1000

    adding functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './assets/investment-calculator-logo.png';
-
+import { useState } from 'react'
 
 const Header = (props) => {
   return (
@@ -11,78 +11,102 @@ const Header = (props) => {
 }
 
 const Form = () => {
+  const submitHandler = () => {
+    console.log('SUBMIT')
+  }
+  const resetHandler = () => {
+    console.log('RESET')
+  }
+  const changeHandler = (input, value) => {
+    console.log('yo')
+  }
   return (
-    <form className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
+    <form className="form" onSubmit={submitHandler}>
+      <div className="input-group">
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            onChange={(event) =>
+              changeHandler('current-savings', event.target.value)}
+            type="number"
+            id="current-savings"
+          />
         </p>
-      </form>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input onChange={changeHandler} type="number" id="yearly-contribution" />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input onChange={changeHandler} type="number" id="expected-return" />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input onChange={changeHandler} type="number" id="duration" />
+        </p>
+      </div>
+      <p className="actions">
+        <button onClick={resetHandler} type="reset" className="buttonAlt">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Calculate
+        </button>
+      </p>
+    </form>
   )
 }
 
 const Result = () => {
   return (
     <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>YEAR NUMBER</td>
+          <td>TOTAL SAVINGS END OF YEAR</td>
+          <td>INTEREST GAINED IN YEAR</td>
+          <td>TOTAL INTEREST GAINED</td>
+          <td>TOTAL INVESTED CAPITAL</td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
 function App() {
+  const [currentSavings, setCurrentSavings] = useState('')
+  const [yearlyContribution, setyearlyContribution] = useState('')
+  const [expectedReturn, setExpectedReturn] = useState('')
+  const [duration, setDuration] = useState('')
+
+  const amountChangeHandler = (event) => {
+    setCurrentSavings(event.target.value)
+    console.log(currentSavings)
+  }
+
   const calculateHandler = (userInput) => {
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
 
     const yearlyData = []; // per-year results
 
-    let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput['expected-return'] / 100;
-    const duration = +userInput['duration'];
+    setCurrentSavings(+userInput['current-savings']); // feel free to change the shape of this input object!
+    setyearlyContribution(+userInput['yearly-contribution']); // as mentioned: feel free to change the shape...
+    setExpectedReturn(+userInput['expected-return'] / 100);
+    setDuration(+userInput['duration']);
 
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
@@ -103,11 +127,9 @@ function App() {
   return (
     <div>
       <Header logo={logo} />
-      <Form />
-
+      <Form amountChangeHandler={amountChangeHandler} calculateHandler={calculateHandler} />
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
-
       <Result />
     </div>
   );

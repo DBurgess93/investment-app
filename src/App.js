9b99c4a0fb261281b917c2780123d5d430d29d78
Daniel Burgess <dan-burgess@hotmail.com>
2023-08-06 13:57:18 +1000

    showing data

--- src/App.js
+++ src/App.js
@@ -104,60 +104,58 @@ const Result = (props) => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>YEARLY</td>
-          <td>TOTAL SAVINGS END OF YEAR</td>
-          <td>INTEREST GAINED IN YEAR</td>
-          <td>TOTAL INTEREST GAINED</td>
-          <td>TOTAL INVESTED CAPITAL</td>
-        </tr>
+        {props.data.map(yearData =>
+          <tr>
+            <td>{yearData.year}</td>
+            <td>{yearData.savingsEndOfYear}</td>
+            <td>{yearData.yearlyInterest}</td>
+            <td>{yearData.savingsEndOfYear}</td>
+            <td>TOTAL INVESTED CAPITAL</td>
+          </tr>
+        )}
       </tbody>
     </table>
   )
 }
 
 function App() {
-  const [results, setResults] = useState(null)
+  const [userInput, setUserInput] = useState(null)
 
   const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
+    setUserInput(userInput)
+  }
 
-    const yearlyData = []; // per-year results
+  const yearlyData = []; // per-year results
 
-    if (userInput) {
-      let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-      const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
-      const expectedReturn = +userInput['expected-return'] / 100;
-      const duration = +userInput['duration'];
+  if (userInput) {
+    let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput['expected-return'] / 100;
+    const duration = +userInput['duration'];
 
-      // The below code calculates yearly results (total savings, interest etc)
-      for (let i = 0; i < duration; i++) {
-        const yearlyInterest = currentSavings * expectedReturn;
-        currentSavings += yearlyInterest + yearlyContribution;
-        yearlyData.push({
-          // feel free to change the shape of the data pushed to the array!
-          year: i + 1,
-          yearlyInterest: yearlyInterest,
-          savingsEndOfYear: currentSavings,
-          yearlyContribution: yearlyContribution,
-        });
-      }
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
     }
+  }
 
-    setResults(yearlyData)
-    // do something with yearlyData ...
-  };
 
   return (
     <div>
       <Header logo={logo} />
       <Form onCalculate={calculateHandler} />
-      {/* Todo: Show below table conditionally (only once result data is available) */}
-      {/* Show fallback text if no data is available */}
-      <Result />
+
+      {!userInput && <p>No investment calculated yet.</p>}
+      {userInput && <Result data={yearlyData} />}
     </div>
   );
 }
-
 export default App;

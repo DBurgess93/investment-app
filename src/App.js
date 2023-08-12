308670938627c85408b0aafe843c1ec0f6f87d8b
Daniel Burgess <dan-burgess@hotmail.com>
2023-08-12 11:48:36 +0930

    Serperate components into seperate files

--- src/App.js
+++ src/App.js
@@ -1,145 +1,8 @@
 import logo from './assets/investment-calculator-logo.png';
+import Form from './components/Form'
+import Header from './components/Header'
+import Results from './components/Results'
 import { useState } from 'react'
-
-const Header = (props) => {
-  return (
-    <header className="header">
-      <img src={props.logo} alt="logo" />
-      <h1>Investment Calculator</h1>
-    </header>
-  )
-}
-
-const initialUserInput = {
-  'current-savings': 10000,
-  'yearly-contribution': 1200,
-  'expected-return': 7,
-  'duration': 10
-}
-
-const Form = (props) => {
-  const [userInput, setUserInput] = useState(initialUserInput)
-
-  const submitHandler = (event) => {
-    event.preventDefault()
-    props.onCalculate(userInput)
-  }
-  const resetHandler = (event) => {
-    setUserInput(initialUserInput)
-  }
-  const changeHandler = (input, value) => {
-    setUserInput((prevInput) => {
-      return {
-        ...prevInput,
-        [input]: value,
-      }
-    })
-  }
-  return (
-    <form className="form" onSubmit={submitHandler}>
-      <div className="input-group">
-        <p>
-          <label htmlFor="current-savings">Current Savings ($)</label>
-          <input
-            onChange={(event) =>
-              changeHandler('current-savings', event.target.value)}
-            value={userInput['current-savings']}
-            type="number"
-            id="current-savings"
-          />
-        </p>
-        <p>
-          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input
-            onChange={(event) =>
-              changeHandler('yearly-contribution', event.target.value)}
-            value={userInput['yearly-contribution']}
-            type="number"
-            id="yearly-contribution" />
-        </p>
-      </div>
-      <div className="input-group">
-        <p>
-          <label htmlFor="expected-return">
-            Expected Interest (%, per year)
-          </label>
-          <input
-            onChange={(event) =>
-              changeHandler('expected-return', event.target.value)}
-            value={userInput['expected-return']}
-            type="number"
-            id="expected-return" />
-        </p>
-        <p>
-          <label htmlFor="duration">Investment Duration (years)</label>
-          <input
-            onChange={(event) =>
-              changeHandler('duration', event.target.value)}
-            value={userInput['duration']}
-            type="number"
-            id="duration" />
-        </p>
-      </div>
-      <p className="actions">
-        <button
-          onClick={resetHandler}
-          type="reset"
-          className="buttonAlt"
-        >
-          Reset
-        </button>
-        <button type="submit" className="button">
-          Calculate
-        </button>
-      </p>
-    </form>
-  )
-}
-
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
-});
-
-const Results = (props) => {
-  return (
-    <table className="result">
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Total Savings</th>
-          <th>Interest (Year)</th>
-          <th>Total Interest</th>
-          <th>Invested Capital</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.data.map((yearData) => (
-          <tr key={yearData.year}>
-            <td>{yearData.year}</td>
-            <td>{formatter.format(yearData.savingsEndOfYear)}</td>
-            <td>{formatter.format(yearData.yearlyInterest)}</td>
-            <td>
-              {formatter.format(
-                yearData.savingsEndOfYear -
-                  props.initialInvestment -
-                  yearData.yearlyContribution * yearData.year
-              )}
-            </td>
-            <td>
-              {formatter.format(
-                props.initialInvestment +
-                  yearData.yearlyContribution * yearData.year
-              )}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  )
-}
 
 function App() {
   const [userInput, setUserInput] = useState(null)
@@ -167,7 +30,6 @@ function App() {
       });
     }
   }
-
 
   return (
     <div>
